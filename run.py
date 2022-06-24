154b482c7624a748396c8f10049d46b9b15e9e46
nicko <ikar2021@hotmail.com>
2022-06-24 23:15:08 +0300

    Add readme; Exit if webcam doesn't exists

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ def show_frame(frame):
 
 
 def draw_sqare(frame, color):
-        cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+    cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
 
 
 def get_cascades():
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     video_capture = cv2.VideoCapture(0)
     while True:
         if not video_capture.isOpened():
-            print("Couldn't find your webcam... Sorry :c")
+            exit("Couldn't find your webcam... Sorry :c")
         _, webcam_frame = video_capture.read()
         gray_frame = cv2.cvtColor(webcam_frame, cv2.COLOR_BGR2GRAY)
         for cascade, color in cascades:
